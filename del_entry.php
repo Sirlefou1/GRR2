09f869b149ab304db274163be87caab43d9d5f41
Florian OTHON <florian@othon.fr>
2014-09-17 10:36:21 +0200

    clean

--- del_entry.php
+++ del_entry.php
@@ -43,7 +43,6 @@
  *
  *
  */
-
 include "include/connect.inc.php";
 include "include/config.inc.php";
 include "include/functions.inc.php";
@@ -51,86 +50,71 @@ include "include/$dbsys.inc.php";
 include_once('include/misc.inc.php');
 include "include/mrbs_sql.inc.php";
 $grr_script_name = "del_entry.php";
-// Settings
 require_once("./include/settings.inc.php");
-//Chargement des valeurs de la table settingS
 if (!loadSettings())
-    die("Erreur chargement settings");
-
-// Session related functions
+	die("Erreur chargement settings");
 require_once("./include/session.inc.php");
-
-// Resume session
-if (!grr_resumeSession()) {
-    header("Location: ./logout.php?auto=1&url=$url");
-    die();
+if (!grr_resumeSession())
+{
+	header("Location: ./logout.php?auto=1&url=$url");
+	die();
 };
-
-// Paramètres langage
 include "include/language.inc.php";
-
 $series = isset($_GET["series"]) ? $_GET["series"] : NULL;
-if (isset($series)) settype($series,"integer");
+if (isset($series))
+	settype($series,"integer");
 $page = verif_page();
-if (isset($_GET["id"])) {
-    $id = $_GET["id"];
-    settype($id,"integer");
-} else {
-    die();
+if (isset($_GET["id"]))
+{
+	$id = $_GET["id"];
+	settype($id,"integer");
 }
-
+else
+	die();
 if ($info = mrbsGetEntryInfo($id))
 {
-    $day   = strftime("%d", $info["start_time"]);
-    $month = strftime("%m", $info["start_time"]);
-    $year  = strftime("%Y", $info["start_time"]);
-
-    $area  = mrbsGetRoomArea($info["room_id"]);
-    $back = "";
-    if (isset($_SERVER['HTTP_REFERER'])) $back = htmlspecialchars($_SERVER['HTTP_REFERER']);
-    if (authGetUserLevel(getUserName(),-1) < 1)
-    {
-        showAccessDenied($day, $month, $year, $area,$back);
-        exit();
-    }
-    if (!getWritable($info["beneficiaire"], getUserName(),$id))
-    {
-        showAccessDenied($day, $month, $year, $area,$back);
-        exit;
-    }
-    if (authUserAccesArea(getUserName(), $area)==0)
-    {
-        showAccessDenied($day, $month, $year, $area,$back);
-        exit();
-    }
-
-    grr_sql_begin();
-    if (getSettingValue("automatic_mail") == 'yes') {
-        $_SESSION['session_message_error'] = send_mail($id,3,$dformat);
-    }
-    // On vérifie les dates
-    $room_id = grr_sql_query1("SELECT ".TABLE_PREFIX."_entry.room_id FROM ".TABLE_PREFIX."_entry, ".TABLE_PREFIX."_room WHERE ".TABLE_PREFIX."_entry.room_id = ".TABLE_PREFIX."_room.id AND ".TABLE_PREFIX."_entry.id='".$id."'");
-    $date_now = time();
-    get_planning_area_values($area); // Récupération des données concernant l'affichage du planning du domaine
-    if ((!(verif_booking_date(getUserName(), $id, $room_id, -1, $date_now, $enable_periods))) or
-    ((verif_booking_date(getUserName(), $id, $room_id, -1, $date_now, $enable_periods)) and ($can_delete_or_create!="y"))
-    )
-    {
-          showAccessDenied($day, $month, $year, $area,$back);
-          exit();
-    }
-
-    $result = mrbsDelEntry(getUserName(), $id, $series, 1);
-    grr_sql_commit();
-    if ($result)
-    {
-        $_SESSION['displ_msg'] = 'yes';
-        Header("Location: ".$page.".php?day=$day&month=$month&year=$year&area=$area&room=".$info["room_id"]);
-        exit();
-    }
+	$day   = strftime("%d", $info["start_time"]);
+	$month = strftime("%m", $info["start_time"]);
+	$year  = strftime("%Y", $info["start_time"]);
+	$area  = mrbsGetRoomArea($info["room_id"]);
+	$back = "";
+	if (isset($_SERVER['HTTP_REFERER']))
+		$back = htmlspecialchars($_SERVER['HTTP_REFERER']);
+	if (authGetUserLevel(getUserName(), -1) < 1)
+	{
+		showAccessDenied($day, $month, $year, $area, $back);
+		exit();
+	}
+	if (!getWritable($info["beneficiaire"], getUserName(), $id))
+	{
+		showAccessDenied($day, $month, $year, $area, $back);
+		exit;
+	}
+	if (authUserAccesArea(getUserName(), $area) == 0)
+	{
+		showAccessDenied($day, $month, $year, $area, $back);
+		exit();
+	}
+	grr_sql_begin();
+	if (getSettingValue("automatic_mail") == 'yes')
+		$_SESSION['session_message_error'] = send_mail($id,3,$dformat);
+	$room_id = grr_sql_query1("SELECT ".TABLE_PREFIX."_entry.room_id FROM ".TABLE_PREFIX."_entry, ".TABLE_PREFIX."_room WHERE ".TABLE_PREFIX."_entry.room_id = ".TABLE_PREFIX."_room.id AND ".TABLE_PREFIX."_entry.id='".$id."'");
+	$date_now = time();
+	get_planning_area_values($area);
+	if ((!(verif_booking_date(getUserName(), $id, $room_id, -1, $date_now, $enable_periods))) || ((verif_booking_date(getUserName(), $id, $room_id, -1, $date_now, $enable_periods)) && ($can_delete_or_create != "y")))
+	{
+		showAccessDenied($day, $month, $year, $area,$back);
+		exit();
+	}
+	$result = mrbsDelEntry(getUserName(), $id, $series, 1);
+	grr_sql_commit();
+	if ($result)
+	{
+		$_SESSION['displ_msg'] = 'yes';
+		Header("Location: ".$page.".php?day=$day&month=$month&year=$year&area=$area&room=".$info["room_id"]);
+		exit();
+	}
 }
-
-// If you got this far then we got an access denied.
 $day   = date("d");
 $month = date("m");
 $year  = date("Y");
--- deletefiles.php
+++ deletefiles.php
@@ -1,24 +1,16 @@
 <?php
-  
-	include "include/connect.inc.php";
-	include "include/mysql.inc.php";
-	include "include/misc.inc.php";
-	
-
-   if (isset($_POST["link"]) && strlen($_POST["link"]) > 0) {
-        // On récupère le nom de fichier
-        $file = $_POST["link"]; 
-        $path = "uploadify/files/";
-
-        // On tente d'effacer le fichier, si il existe et si il est effacer
-        // le retour sera à true
-        if (unlink($path.$file)) {
-            $sql = "DELETE from ".TABLE_PREFIX."_files WHERE nom = '".$file."' ";
-			grr_sql_command($sql);
-            echo "deleted";
-        }
-    }
-
-
-
+include "include/connect.inc.php";
+include "include/mysql.inc.php";
+include "include/misc.inc.php";
+if (isset($_POST["link"]) && strlen($_POST["link"]) > 0)
+{
+	$file = $_POST["link"];
+	$path = "uploadify/files/";
+	if (unlink($path.$file))
+	{
+		$sql = "DELETE from ".TABLE_PREFIX."_files WHERE nom = '".$file."' ";
+		grr_sql_command($sql);
+		echo "deleted";
+	}
+}
 ?>
