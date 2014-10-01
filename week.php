d73af10e48c93fe42b86283953a6dc32e4d4a263
Florian OTHON <florian@othon.fr>
2014-10-01 15:49:38 +0200

    fix affichage

--- week.php
+++ week.php
@@ -204,7 +204,7 @@ if (verif_display_fiche_ressource(getUserName(), $room) && $_GET['pview'] != 1)
 	echo "<a href='javascript:centrerpopup(\"view_room.php?id_room=$room\",600,480,\"scrollbars=yes,statusbar=no,resizable=yes\")' title=\"".get_vocab("fiche_ressource")."\">
 <span class=\"glyphcolor glyphicon glyphicon-search\"></span></a>";
 if (authGetUserLevel(getUserName(),$room) > 2 && $_GET['pview'] != 1)
-	echo "<a href='admin_edit_room.php?room=$room'><span class=\"glyphcolor glyphicon glyphicon-cog\"></span></a>";
+	echo "<a href='admin_edit_room.php?room=$room'><span class=\"glyphcolor glyphalign glyphicon glyphicon-cog\"></span></a>";
 affiche_ressource_empruntee($room);
 if ($this_statut_room == "0")
 	echo "<br /><span class=\"texte_ress_tempo_indispo\">".get_vocab("ressource_temporairement_indisponible")."</span>";
