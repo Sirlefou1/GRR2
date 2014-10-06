1398e5b9871638206e42410db5e482cc69e425f0
Florian OTHON <florian@othon.fr>
2014-10-06 13:49:23 +0200

    chargement + mise en forme

--- chargement.php
+++ chargement.php
@@ -1,6 +1,8 @@
 <?php
 echo "
 <div id=\"chargement\">
-   <p><img src=\"images/chargement.gif\"/></p>
+	<div class=\"progress\">
+		<div class=\"progress-bar progress-bar-info progress-bar-striped\" role=\"progressbar\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 100%\"></div>
+	</div>
 </div>";
 ?>
