1bfc8951c395893f876b655b9e5bf57814b6cfdd
Florian OTHON <florian@othon.fr>
2014-10-10 09:51:08 +0200

    fix

--- edit_entry.php
+++ edit_entry.php
@@ -1019,6 +1019,18 @@ echo "\n<!-- ************* Periodic edition ***************** -->\n";
 $weeklist = array("unused","every week","week 1/2","week 1/3","week 1/4","week 1/5");
 if (($edit_type == "series") || (isset($flag_periodicite)))
 {
+	echo "
+	<tr>
+		<td id=\"ouvrir\" style=\"cursor: inherit\" align=\"center\" class=\"fontcolor4\">
+			<span class=\"bground\"><b><a href='javascript:clicMenu(1);check_5()'>".get_vocab("click_here_for_series_open")."</a></b></span>
+		</td>
+	</tr>
+	<tr>
+		<td style=\"display:none; cursor: inherit\" id=\"fermer\" align=\"center\" class=\"fontcolor4\">
+			<span class=\"bground\"><b><a href='javascript:clicMenu(1);check_5()'>".get_vocab("click_here_for_series_close")."</a></b></span>
+		</td>
+	</tr>
+	";
 	echo "<tr><td><table id=\"menu1\">\n ";
 	echo "<tr><td class=\"F\"><b>".get_vocab("rep_type")."</b></td></tr><tr><td class=\"CL\">\n";
 	echo "<table border=\"0\"  width=\"100%\" >\n";
@@ -1026,7 +1038,7 @@ if (($edit_type == "series") || (isset($flag_periodicite)))
 		$max = 7;
 	else
 		$max = 6;
-	for ($i = 0; $i<$max ; $i++)
+	for ($i = 0; $i < $max ; $i++)
 	{
 		if ($i != 5)
 		{
