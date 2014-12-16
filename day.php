809cb81954baa292ce017b5e31d5f43e386194f8
Florian OTHON <florian@othon.fr>
2014-12-16 15:28:07 +0100

    opti

--- day.php
+++ day.php
@@ -129,40 +129,40 @@ else
 {
 	for ($i = 0; ($row = grr_sql_row($res, $i)); $i++)
 	{
-		$start_t = max(round_t_down($row[1], $resolution, $am7), $am7);
-		$end_t = min(round_t_up($row[2], $resolution, $am7) - $resolution, $pm7);
-		$cellules[$row[4]] = ($end_t - $start_t) / $resolution + 1;
-		$compteur[$row[4]] = 0;
+		$start_t = max(round_t_down($row['1'], $resolution, $am7), $am7);
+		$end_t = min(round_t_up($row['2'], $resolution, $am7) - $resolution, $pm7);
+		$cellules[$row['4']] = ($end_t - $start_t) / $resolution + 1;
+		$compteur[$row['4']] = 0;
 		for ($t = $start_t; $t <= $end_t; $t += $resolution)
 		{
-			$today[$row[0]][$t]["id"]    		= $row[4];
-			$today[$row[0]][$t]["color"] 		= $row[5];
-			$today[$row[0]][$t]["data"]  		= "";
-			$today[$row[0]][$t]["who"]			= "";
-			$today[$row[0]][$t]["statut"] 		= $row[7];
-			$today[$row[0]][$t]["moderation"] 	= $row[10];
-			$today[$row[0]][$t]["option_reser"] = $row[9];
-			$today[$row[0]][$t]["description"] 	= affichage_resa_planning($row[8], $row[4]);
+			$today[$row['0']][$t]["id"]    		= $row['4'];
+			$today[$row['0']][$t]["color"] 		= $row['5'];
+			$today[$row['0']][$t]["data"]  		= "";
+			$today[$row['0']][$t]["who"]			= "";
+			$today[$row['0']][$t]["statut"] 		= $row['7'];
+			$today[$row['0']][$t]["moderation"] 	= $row['10'];
+			$today[$row['0']][$t]["option_reser"] = $row['9'];
+			$today[$row['0']][$t]["description"] 	= affichage_resa_planning($row['8'], $row['4']);
 		}
-		if ($row[1] < $am7)
+		if ($row['1'] < $am7)
 		{
-			$today[$row[0]][$am7]["data"] = affichage_lien_resa_planning($row[3], $row[4]);
+			$today[$row['0']][$am7]["data"] = affichage_lien_resa_planning($row['3'], $row['4']);
 			if ($settings->get("display_info_bulle") == 1)
-				$today[$row[0]][$am7]["who"] = get_vocab("reservation au nom de").affiche_nom_prenom_email($row[6], $row[11], "nomail");
+				$today[$row['0']][$am7]["who"] = get_vocab("reservation au nom de").affiche_nom_prenom_email($row['6'], $row['11'], "nomail");
 			else if ($settings->get("display_info_bulle") == 2)
-				$today[$row[0]][$am7]["who"] = $row[8];
+				$today[$row['0']][$am7]["who"] = $row[8];
 			else
-				$today[$row[0]][$am7]["who"] = "";
+				$today[$row['0']][$am7]["who"] = "";
 		}
 		else
 		{
-			$today[$row[0]][$start_t]["data"] = affichage_lien_resa_planning($row[3], $row[4]);
+			$today[$row['0']][$start_t]["data"] = affichage_lien_resa_planning($row['3'], $row['4']);
 			if ($settings->get("display_info_bulle") == 1)
-				$today[$row[0]][$start_t]["who"] = get_vocab("reservation au nom de").affiche_nom_prenom_email($row[6], $row[11]);
+				$today[$row['0']][$start_t]["who"] = get_vocab("reservation au nom de").affiche_nom_prenom_email($row['6'], $row['11']);
 			else if ($settings->get("display_info_bulle") == 2)
-				$today[$row[0]][$start_t]["who"] = $row[8];
+				$today[$row['0']][$start_t]["who"] = $row['8'];
 			else
-				$today[$row[0]][$start_t]["who"] = "";
+				$today[$row['0']][$start_t]["who"] = "";
 		}
 	}
 }
@@ -200,10 +200,10 @@ else
 			}
 		}
 		$sh = getSchoolHolidays($tt, $year);
-		if ($sh[0] == true)
+		if ($sh['0'] == true)
 		{
 			$class .= "vacance ";
-			$title = " ".$sh[1];
+			$title = " ".$sh['1'];
 		}
 		if ($ferie_true)
 			$class .= "ferie ";
@@ -215,69 +215,69 @@ else
 		include "include/trailer.inc.php";
 		echo "</td><td class=\"right\"><button type=\"button\" class=\"btn btn-default btn-xs\" onclick=\"charger();javascript: location.href='day.php?year=$ty&amp;month=$tm&amp;day=$td&amp;area=$area';\"> ".get_vocab('dayafter')."  <span class=\"glyphicon glyphicon-forward\"></span></button></td></tr></table>";
 	}
-	echo "<h4 class=\"titre\">" . ucfirst(utf8_strftime($dformat, $am7));
+	echo '<h4 class="titre">' . ucfirst(utf8_strftime($dformat, $am7));
 	if ($settings->get("jours_cycles_actif") == "Oui" && intval($jour_cycle) >- 1)
 	{
 		if (intval($jour_cycle) > 0)
-			echo " - ".get_vocab("rep_type_6")." ".$jour_cycle;
+			echo ' - '.get_vocab("rep_type_6")." ".$jour_cycle;
 		else
-			echo " - ".$jour_cycle;
+			echo ' - '.$jour_cycle;
 	}
-	echo " ".ucfirst($this_area_name)." - ".get_vocab("all_areas")."</h4>";
-	echo " </div>\n";
+	echo ' '.ucfirst($this_area_name).' - '.get_vocab("all_areas").'</h4>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
 	if (isset($_GET['precedent']))
 	{
 		if ($_GET['pview'] == 1 && $_GET['precedent'] == 1)
-			echo "<span id=\"lienPrecedent\"><button type=\"button\" class=\"btn btn-default btn-xs\" onclick=\"charger();javascript:history.back();\">Précedent</button></span>";
+			echo '<span id="lienPrecedent"><button type="button" class="btn btn-default btn-xs" onclick="charger();javascript:history.back();">Précedent</button></span>'.PHP_EOL;
 	}
-	echo "<div class=\"contenu_planning\">" ;
-	echo "<table class=\"table-bordered table-striped\">";
-	echo "<tr>\n<th style=\"width:5%;\">";
+	echo '<div class="contenu_planning">'.PHP_EOL;
+	echo '<table class="table-bordered table-striped">'.PHP_EOL;
+	echo '<tr>'.PHP_EOL.'<th style="width:5%;">'.PHP_EOL;
 	if ($enable_periods == 'y')
 		echo get_vocab("period");
 	else
 		echo get_vocab("time");
-	echo  "</th>";
+	echo  '</th>'.PHP_EOL;
 	$room_column_width = (int)(90 / grr_sql_count($res));
 	$nbcol = 0;
 	$rooms = array();
 	$a = 0;
 	for ($i = 0; ($row = grr_sql_row($res, $i)); $i++)
 	{
-		$id_room[$i] = $row[2];
+		$id_room[$i] = $row['2'];
 		$nbcol++;
 		if (verif_acces_ressource(getUserName(), $id_room[$i]))
 		{
-			$room_name[$i] = $row[0];
-			$statut_room[$id_room[$i]] =  $row[4];
-			$statut_moderate[$id_room[$i]] =  $row[7];
+			$room_name[$i] = $row['0'];
+			$statut_room[$id_room[$i]] =  $row['4'];
+			$statut_moderate[$id_room[$i]] =  $row['7'];
 			$acces_fiche_reservation = verif_acces_fiche_reservation(getUserName(), $id_room[$i]);
-			if ($row[1]  && $_GET['pview'] != 1)
-				$temp = "<br /><span class=\"small\">($row[1] ".($row[1] > 1 ? get_vocab("number_max2") : get_vocab("number_max")).")</span>";
+			if ($row['1']  && $_GET['pview'] != 1)
+				$temp = '<br /><span class="small">('.$row['1'].' '.($row['1'] > 1 ? get_vocab("number_max2") : get_vocab("number_max")).')</span>'.PHP_EOL;
 			else
-				$temp = "";
+				$temp = '';
 			if ($statut_room[$id_room[$i]] == "0"  && $_GET['pview'] != 1)
-				$temp .= "<br /><span class=\"texte_ress_tempo_indispo\">".get_vocab("ressource_temporairement_indisponible")."</span>";
+				$temp .= '<br /><span class="texte_ress_tempo_indispo">'.get_vocab("ressource_temporairement_indisponible").'</span>'.PHP_EOL;
 			if ($statut_moderate[$id_room[$i]] == "1"  && $_GET['pview'] != 1)
-				$temp .= "<br /><span class=\"texte_ress_moderee\">".get_vocab("reservations_moderees")."</span>";
-			echo "<th style=\"width:$room_column_width%;\" ";
+				$temp .= '<br /><span class="texte_ress_moderee">'.get_vocab("reservations_moderees").'</span>'.PHP_EOL;
+			echo '<th style="width:'.$room_column_width.'%;" ';
 			if ($statut_room[$id_room[$i]] == "0")
-				echo "class='avertissement' ";
+				echo 'class="avertissement" ';
 			$a = $a + 1;
-			echo "><a id=\"afficherBoutonSelection$a\" class=\"lienPlanning\" href=\"#\" onclick=\"afficherMoisSemaine($a)\" style=\"display:inline;\">".htmlspecialchars($row[0])."</a>\n";
-			echo "<a id=\"cacherBoutonSelection$a\" class=\"lienPlanning\" href=\"#\" onclick=\"cacherMoisSemaine($a)\" style=\"display:none;\">".htmlspecialchars($row[0])."</a>\n";
-			if (htmlspecialchars($row[3]).$temp != '')
+			echo '><a id="afficherBoutonSelection'.$a.'" class="lienPlanning" href="#" onclick="afficherMoisSemaine('.$a.')" style="display:inline;">'.htmlspecialchars($row['0']).'</a>'.PHP_EOL;
+			echo '<a id="cacherBoutonSelection'.$a.'" class="lienPlanning" href="#" onclick="cacherMoisSemaine('.$a.')" style="display:none;">'.htmlspecialchars($row['0']).'</a>'.PHP_EOL;
+			if (htmlspecialchars($row['3']).$temp != '')
 			{
-				if (htmlspecialchars($row[3]) != '')
+				if (htmlspecialchars($row['3']) != '')
 					$saut = '<br />';
 				else
-					$saut = "";
-				echo $saut.htmlspecialchars($row[3]).$temp."\n";
+					$saut = '';
+				echo $saut.htmlspecialchars($row['3']).$temp."\n";
 			}
 			echo '<br />';
 			if (verif_display_fiche_ressource(getUserName(), $id_room[$i]) && $_GET['pview'] != 1)
-				echo "<a href='javascript:centrerpopup(\"view_room.php?id_room=$id_room[$i]\",600,480,\"scrollbars=yes,statusbar=no,resizable=yes\")' title=\"".get_vocab("fiche_ressource")."\">
-			<span class=\"glyphcolor glyphicon glyphicon-search\"></span></a>";
+				echo '<a href="javascript:centrerpopup(\'view_room.php?id_room='.$id_room[$i].'\',600,480,"scrollbars=yes,statusbar=no,resizable=yes")" title="'.get_vocab("fiche_ressource").'\">
+			<span class="glyphcolor glyphicon glyphicon-search"></span></a>'.PHP_EOL;
 			if (authGetUserLevel(getUserName(),$id_room[$i]) > 2 && $_GET['pview'] != 1)
 				echo '<a href="admin_edit_room.php?room='.$id_room[$i].'"><span class="glyphcolor glyphicon glyphicon-cog"></span></a><br/>'.PHP_EOL;
 			affiche_ressource_empruntee($id_room[$i]);
@@ -286,15 +286,15 @@ else
 			echo '<input type="button" class="btn btn-default btn-xs" title="'.htmlspecialchars(get_vocab("see_month_for_this_room")).'" onclick="charger();javascript: location.href=\'month.php?year='.$year.'&amp;month='.$month.'&amp;day='.$day.'&amp;room='.$id_room[$i].'\';" value="'.get_vocab('month').'"/>'.PHP_EOL;
 			echo '</span>'.PHP_EOL;
 			echo '</th>'.PHP_EOL;
-			if (htmlspecialchars($row[3]).$temp != '')
+			if (htmlspecialchars($row['3']).$temp != '')
 			{
-				if (htmlspecialchars($row[3]) != '')
+				if (htmlspecialchars($row['3']) != '')
 					$saut = '<br />';
 				else
 					$saut = '';
 			}
-			$rooms[] = $row[2];
-			$delais_option_reservation[$row[2]] = $row[6];
+			$rooms[] = $row['2'];
+			$delais_option_reservation[$row['2']] = $row['6'];
 		}
 	}
 	if (count($rooms) == 0)
@@ -428,11 +428,11 @@ else
 						$res = grr_sql_query($sql);
 						for ($i = 0; ($row = grr_sql_row($res, $i)); $i++)
 						{
-							$type_name  = $row[0];
-							$start_time = $row[1];
-							$end_time   = $row[2];
-							$clef 		= $row[3];
-							$courrier	= $row[4];
+							$type_name  = $row['0'];
+							$start_time = $row['1'];
+							$end_time   = $row['2'];
+							$clef 		= $row['3'];
+							$courrier	= $row['4'];
 							echo '<br/>',date('H:i', $start_time),get_vocab("to"),date('H:i', $end_time),'<br/>';
 							if ($type_name != -1)
 								echo  $type_name;
