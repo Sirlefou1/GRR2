99a035a2716584f0bc028717fcf30ba9a0ba50b6
Florian OTHON <florian@othon.fr>
2014-12-18 09:21:06 +0100

    Clean up + Opti + W3c month.php

--- month.php
+++ month.php
@@ -160,54 +160,43 @@ if ($_GET['pview'] != 1)
 else
 	echo '<div id="print_planning">'.PHP_EOL;
 echo '<div class="titre_planning">'.PHP_EOL;
-echo '<table width="100%">'.PHP_EOL;
+echo '<table class="table-header">'.PHP_EOL;
 if ((!isset($_GET['pview'])) || ($_GET['pview'] != 1))
 {
 	echo '<tr>'.PHP_EOL;
-	echo '<td align="left">'.PHP_EOL;
+	echo '<td class="left">'.PHP_EOL;
 	echo '<button class="btn btn-default btn-xs" onclick="charger();javascript: location.href=\'month.php?year='.$yy.'&amp;month='.$ym.'&amp;room='.$room.'\';"><span class="glyphicon glyphicon-backward"></span> '.get_vocab("monthbefore").'</button>'.PHP_EOL;
 	echo '</td>'.PHP_EOL;
 	echo '<td>'.PHP_EOL;
 	include "include/trailer.inc.php";
 	echo '</td>'.PHP_EOL;
-	echo '<td align="right">'.PHP_EOL;
-	echo '<button class="btn btn-default btn-xs" onclick="charger();javascript: location.href=\'month.php?year='.$ty.'&amp;month='.$tm.'&amp;room='.$room.'\';"> '.get_vocab('monthafter').'  <span class=\"glyphicon glyphicon-forward\"></span></button>'.PHP_EOL;
+	echo '<td class="right">'.PHP_EOL;
+	echo '<button class="btn btn-default btn-xs" onclick="charger();javascript: location.href=\'month.php?year='.$ty.'&amp;month='.$tm.'&amp;room='.$room.'\';"> '.get_vocab('monthafter').'  <span class="glyphicon glyphicon-forward"></span></button>'.PHP_EOL;
 	echo '</td>'.PHP_EOL;
 	echo '</tr>'.PHP_EOL;
 	echo '</table>'.PHP_EOL;
 }
-echo '<tr>'.PHP_EOL;
-echo '<td>'.PHP_EOL;
-echo '<h4 class="titre"> '. ucfirst(utf8_strftime("%B %Y", $month_start)).''.ucfirst($this_area_name).' - '.$this_room_name.' '.$this_room_name_des.'</h4>'.PHP_EOL;
-echo '</td>'.PHP_EOL;
-echo '</tr>'.PHP_EOL;
-echo '</table>'.PHP_EOL;
+echo '<h4 class="titre"> '. ucfirst($this_area_name).' - '.$this_room_name.' '.$this_room_name_des.'<br>'.ucfirst(utf8_strftime("%B %Y", $month_start)).'</h4>'.PHP_EOL;
 if (verif_display_fiche_ressource(getUserName(), $room) && $_GET['pview'] != 1)
-	echo "<a href='javascript:centrerpopup(\"view_room.php?id_room=$room\",600,480,\"scrollbars=yes,statusbar=no,resizable=yes\")' title=\"".get_vocab("fiche_ressource")."\">
-<span class=\"glyphcolor glyphicon glyphicon-search\"></span></a>";
+	echo '<a href="javascript:centrerpopup(\'view_room.php?id_room=',$room,'\',600,480,\'scrollbars=yes,statusbar=no,resizable=yes\')" title="',get_vocab("fiche_ressource"),'"><span class="glyphcolor glyphicon glyphicon-search"></span></a>';
 if (authGetUserLevel(getUserName(),$room) > 2 && $_GET['pview'] != 1)
-	echo "<a href='admin_edit_room.php?room=$room'><span class=\"glyphcolor glyphicon glyphicon-cog\"></span></a>";
+	echo '<a href="admin_edit_room.php?room=',$room,'"><span class="glyphcolor glyphicon glyphicon-cog"></span></a>';
 affiche_ressource_empruntee($room);
 if ($this_statut_room == "0")
-	echo "<br /><span class=\"texte_ress_tempo_indispo\">".get_vocab("ressource_temporairement_indisponible")."</span>";
+	echo '<br><span class="texte_ress_tempo_indispo">',get_vocab("ressource_temporairement_indisponible"),'</span>';
 if ($this_moderate_room == "1")
-	echo "<br /><span class=\"texte_ress_moderee\">".get_vocab("reservations_moderees")."</span>";
-echo " </div>\n";
-
+	echo '<br><span class="texte_ress_moderee">',get_vocab("reservations_moderees"),'</span>';
+echo '</div>',PHP_EOL;
 if (isset($_GET['precedent']))
 {
 	if ($_GET['pview'] == 1 && $_GET['precedent'] == 1)
 	{
-		echo "<span id=\"lienPrecedent\">
-		<button class=\"btn btn-default btn-xs\" onclick=\"charger();javascript:history.back();\">Précedent</button>
-	</span>";
-}
+		echo '<span id="lienPrecedent">',PHP_EOL,'<button class="btn btn-default btn-xs" onclick="charger();javascript:history.back();">Précedent</button>',PHP_EOL,'</span>',PHP_EOL;
+	}
 }
 if ($this_area_show_comment == "y" && $_GET['pview'] != 1 && ($this_area_comment != "") && ($this_area_comment != -1))
-	echo "<div style=\"text-align:center;\">".$this_area_comment."</div>";
-echo " <div class=\"contenu_planning\">";
-if ($debug_flag)
-	echo "<p>DEBUG: month=$month year=$year start=$weekday_start range=$month_start:$month_end\n";
+	echo '<div style="text-align:center;">',$this_area_comment,'</div>',PHP_EOL;
+echo '<div class="contenu_planning">',PHP_EOL;
 $all_day = preg_replace("/ /", " ", get_vocab("all_day2"));
 $sql = "SELECT start_time, end_time, id, name, beneficiaire, description, type, moderate, beneficiaire_ext
 FROM ".TABLE_PREFIX."_entry
@@ -220,8 +209,6 @@ if (!$res)
 else
 	for ($i = 0; ($row = grr_sql_row($res, $i)); $i++)
 	{
-		if ($debug_flag)
-			echo "<br />DEBUG: result $i, id $row[2], starts $row[0], ends $row[1]\n";
 		$t = max((int)$row[0], $month_start);
 		$end_t = min((int)$row[1], $month_end);
 		$day_num = date("j", $t);
@@ -231,7 +218,6 @@ else
 			$midnight = mktime(0, 0, 0, $month, $day_num, $year);
 		while ($t < $end_t)
 		{
-			if ($debug_flag) echo "<br />DEBUG: Entry $row[2] day $day_num\n";
 			$d[$day_num]["id"][] = $row[2];
 			if (Settings::get("display_info_bulle") == 1)
 				$d[$day_num]["who"][] = get_vocab("reservee au nom de").affiche_nom_prenom_email($row[4],$row[8],"nomail");
@@ -311,46 +297,30 @@ else
 					break;
 				}
 			}
-				//Seulement si l'heure de fin est pares minuit, on continue le jour prochain.
+			//Seulement si l'heure de fin est pares minuit, on continue le jour prochain.
 			if ($row[1] <= $midnight_tonight)
 				break;
 			$day_num++;
 			$t = $midnight = $midnight_tonight;
 		}
 	}
-	if ($debug_flag)
-	{
-		echo "<p>DEBUG: Array of month day data:<p><pre>\n";
-		for ($i = 1; $i <= $days_in_month; $i++)
-		{
-			if (isset($d[$i]["id"]))
-			{
-				$n = count($d[$i]["id"]);
-				echo "Day $i has $n entries:\n";
-				for ($j = 0; $j < $n; $j++)
-					echo "  ID: " . $d[$i]["id"][$j] .
-				" Data: " . $d[$i]["data"][$j] . "\n";
-			}
-		}
-		echo "</pre>\n";
-	}
-	echo "<table class=\"table-bordered table-striped\">\n<tr>";
+	echo '<table class="table-bordered table-striped">',PHP_EOL,'<tr>',PHP_EOL;
 	for ($weekcol = 0; $weekcol < 7; $weekcol++)
 	{
 		$num_week_day = ($weekcol + $weekstarts) % 7;
 		if ($display_day[$num_week_day] == 1)
-			echo "<th style=\"width:14%;\">" . day_name(($weekcol + $weekstarts) % 7) . "</th>";
+			echo '<th style="width:14%;">',day_name(($weekcol + $weekstarts) % 7),'</th>',PHP_EOL;
 	}
-	echo "</tr>\n";
+	echo '</tr>',PHP_EOL;
 	$weekcol = 0;
 	if ($weekcol != $weekday_start)
 	{
-		echo "<tr>";
+		echo '<tr>',PHP_EOL;
 		for ($weekcol = 0; $weekcol < $weekday_start; $weekcol++)
 		{
 			$num_week_day = ($weekcol + $weekstarts) % 7;
 			if ($display_day[$num_week_day] == 1)
-				echo "<td class=\"cell_month_o\"> </td>\n";
+				echo '<td class="cell_month_o">',PHP_EOL,'</td>',PHP_EOL;
 		}
 	}
 	$ferie = getHolidays($year);
@@ -363,7 +333,7 @@ else
 		$name_day = ucfirst(utf8_strftime("%d", $t));
 		$jour_cycle = grr_sql_query1("SELECT Jours FROM ".TABLE_PREFIX."_calendrier_jours_cycle WHERE DAY='$t'");
 		if ($weekcol == 0)
-			echo "<tr>\n";
+			echo '<tr>',PHP_EOL;
 		if ($display_day[$num_week_day] == 1)
 		{
 			if (Settings::get("show_holidays") == "Oui")
@@ -380,26 +350,26 @@ else
 				$sh = getSchoolHolidays($t, $year);
 				if ($sh[0] == true)
 				{
-					$class .= "vacance ";
-					$title = " ".$sh[1];
+					$class .= 'vacance ';
+					$title = ' '.$sh[1];
 				}
 				if ($ferie_true)
-					$class .= "ferie ";
+					$class .= 'ferie ';
 			}
-			echo "<td valign=\"top\" class=\"cell_month\">\n<div class=\"monthday ".$class."\"><a title=\"".htmlspecialchars(get_vocab("see_all_the_rooms_for_the_day")).$title."\" href=\"day.php?year=$year&amp;month=$month&amp;day=$cday&amp;area=$area\">".$name_day;
+			echo '<td class="cell_month">',PHP_EOL,'<div class="monthday ',$class,'">',PHP_EOL,'<a title="',htmlspecialchars(get_vocab("see_all_the_rooms_for_the_day")),$title,'" href="day.php?year=',$year,'&amp;month=',$month,'&amp;day=',$cday,'&amp;area=',$area,'">',$name_day;
 			if (Settings::get("jours_cycles_actif") == "Oui" && intval($jour_cycle) >- 1)
 			{
 				if (intval($jour_cycle) > 0)
-					echo " - ".get_vocab("rep_type_6")." ".$jour_cycle;
+					echo ' - ',get_vocab("rep_type_6"),' ',$jour_cycle;
 				else
-					echo " - ".$jour_cycle;
+					echo ' - ',$jour_cycle;
 			}
-			echo "</a></div>\n";
+			echo '</a>',PHP_EOL,'</div>',PHP_EOL;
 			if (est_hors_reservation(mktime(0, 0, 0, $month, $cday, $year), $area))
 			{
-				echo "<div class=\"empty_cell\">";
-				echo "<img src=\"img_grr/stop.png\" alt=\"".get_vocab("reservation_impossible")."\" title=\"".get_vocab("reservation_impossible")."\" width=\"16\" height=\"16\" class=\"".$class_image."\" />";
-				echo "</div>\n";
+				echo '<div class="empty_cell">',PHP_EOL;
+				echo '<img src="img_grr/stop.png" alt="',get_vocab("reservation_impossible"),'" title="',get_vocab("reservation_impossible"),'" width="16" height="16" class="',$class_image,'" />',PHP_EOL;
+				echo '</div>',PHP_EOL;
 			}
 			else
 			{
@@ -413,34 +383,33 @@ else
 							echo " ...\n";
 							break;
 						}
-						echo "\n<table width='100%' border='0'><tr>\n";
+						echo '<table class="table-noborder">',PHP_EOL,'<tr>',PHP_EOL;
 						tdcell($d[$cday]["color"][$i]);
-						echo "<span class=\"small_planning\">";
+						echo '<span class="small_planning">';
 						if ($acces_fiche_reservation)
 						{
 							if (Settings::get("display_level_view_entry") == 0)
 							{
-								$currentPage ='month_all2';
+								$currentPage = 'month_all2';
 								$id = $d[$cday]["id"][$i];
-								echo "<a title=\"".htmlspecialchars($d[$cday]["who"][$i])."\" data-width=\"675\" onclick=\"request($id,$cday,$month,$year,'$currentPage',readData);\" data-rel=\"popup_name\" class=\"poplight\">";
+								echo '<a title="',htmlspecialchars($d[$cday]["who"][$i]),'" data-width="675" onclick="request(',$id,',',$cday,',',$month,',',$year,',\'',$currentPage,'\',readData);" data-rel="popup_name" class="poplight">';
 							}
 							else
 							{
-								echo "<a class=\"lienCellule\" title=\"".htmlspecialchars($d[$cday]["who"][$i])."\" href=\"view_entry.php?id=" . $d[$cday]["id"][$i]
-								. "&amp;day=$cday&amp;month=$month&amp;year=$year&amp;page=month\">";
+								echo '<a class="lienCellule" title="',htmlspecialchars($d[$cday]["who"][$i]),'" href="view_entry.php?id=',$d[$cday]["id"][$i],'&amp;day=',$cday,'&amp;month=',$month,'&amp;year=',$year,'&amp;page=month">';
 							}
 						}
-						echo $d[$cday]["data"][$i]. "<br/>";
+						echo $d[$cday]["data"][$i],'<br/>';
 						if ((isset($d[$cday]["moderation"][$i])) && ($d[$cday]["moderation"][$i] == 1))
-							echo " <img src=\"img_grr/flag_moderation.png\" alt=\"".get_vocab("en_attente_moderation")."\" title=\"".get_vocab("en_attente_moderation")."\" class=\"image\" /> \n";
-						echo $d[$cday]["who1"][$i]."<br/>";
+							echo '<img src="img_grr/flag_moderation.png" alt="',get_vocab("en_attente_moderation"),'" title="',get_vocab("en_attente_moderation"),'" class="image" />',PHP_EOL;
+						echo $d[$cday]["who1"][$i],'<br/>';
 						$Son_GenreRepeat = grr_sql_query1("SELECT type_name FROM ".TABLE_PREFIX."_type_area ,".TABLE_PREFIX."_entry  WHERE  ".TABLE_PREFIX."_entry.id= ".$d[$cday]["id"][$i]." AND ".TABLE_PREFIX."_entry.type= ".TABLE_PREFIX."_type_area.type_letter");
-						echo $Son_GenreRepeat."<br/>";
+						echo $Son_GenreRepeat,'<br/>';
 						if ($d[$cday]["description"][$i] != "")
-							echo "<br /><i>(".$d[$cday]["description"][$i].")</i>";
+							echo '<br /><i>(',$d[$cday]["description"][$i],')</i>';
 						if ($acces_fiche_reservation)
-							echo " </a>";
-						echo "</span></td></tr></table>";
+							echo '</a>',PHP_EOL;
+						echo '</span>',PHP_EOL,'</td>',PHP_EOL,'</tr>',PHP_EOL,'</table>',PHP_EOL;
 					}
 				}
 				$date_now = time();
@@ -456,11 +425,11 @@ else
 						(($this_statut_room == "0") && (authGetUserLevel(getUserName(),$room) > 2)))
 					&& $_GET['pview'] != 1)
 				{
-					echo "<div class=\"empty_cell\">";
+					echo '<div class="empty_cell">',PHP_EOL;
 					if ($enable_periods == 'y')
-						echo "<a href=\"edit_entry.php?room=".$room."&amp;period=&amp;year=$year&amp;month=$month&amp;day=$cday&amp;page=month\" title=\"".get_vocab("cliquez_pour_effectuer_une_reservation")."\"><span class=\"glyphicon glyphicon-plus\"></span></a>";
+						echo '<a href="edit_entry.php?room=',$room,'&amp;period=&amp;year=',$year,'&amp;month=',$month,'&amp;day=',$cday,'&amp;page=month" title="',get_vocab("cliquez_pour_effectuer_une_reservation"),'"><span class="glyphicon glyphicon-plus"></span></a>',PHP_EOL;
 					else
-						echo "<a href=\"edit_entry.php?room=".$room."&amp;hour=$hour&amp;minute=0&amp;year=$year&amp;month=$month&amp;day=$cday&amp;page=month\" title=\"".get_vocab("cliquez_pour_effectuer_une_reservation")."\"><span class=\"glyphicon glyphicon-plus\"></span></a>";
+						echo '<a href="edit_entry.php?room=',$room,'&amp;hour=',$hour,'&amp;minute=0&amp;year=',$year,'&amp;month=',$month,'&amp;day=',$cday,'&amp;page=month" title="',get_vocab("cliquez_pour_effectuer_une_reservation"),'"><span class="glyphicon glyphicon-plus"></span></a>',PHP_EOL;
 					echo '</div>'.PHP_EOL;
 				}
 				else
